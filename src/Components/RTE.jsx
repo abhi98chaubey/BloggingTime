67b1b161ac8577525539663149ed41ab147bcb19
Abhishek98chaubey <abhishek81logan@gmail.com>
2024-02-02 14:47:53 +0530

    tiny board api fetching

--- src/Components/RTE.jsx
+++ src/Components/RTE.jsx
@@ -14,6 +14,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     render={({field: {onChange}}) => (
         <Editor
         initialValue={defaultValue}
+        apiKey='dk8zxu2af306wplm2luqfzuxst27lnal3x60re4a5e5rtlh4'
         init={{
             initialValue: defaultValue,
             height: 500,
