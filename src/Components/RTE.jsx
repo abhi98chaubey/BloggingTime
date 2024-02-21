f915105c4602f7a3c29d2ca9114eaa9838469845
Abhishek98chaubey <abhishek81logan@gmail.com>
2024-02-21 17:02:55 +0530

    ready for deplyment

--- src/Components/RTE.jsx
+++ src/Components/RTE.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller } from 'react-hook-form';
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { Controller } from 'react-hook-form';
 
-
-export default function RTE({name, control, label, defaultValue =""}) {
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
-    <div className='w-full'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+    <div className='w-full'>
+      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
 
-    <Controller
-    name={name || "content"}
-    control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        initialValue={defaultValue}
-        apiKey='dk8zxu2af306wplm2luqfzuxst27lnal3x60re4a5e5rtlh4'
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            value={defaultValue}
+            apiKey='dk8zxu2af306wplm2luqfzuxst27lnal3x60re4a5e5rtlh4'
+            init={{
+              height: 500,
+              menubar: true,
+              plugins: [
                 "advlist",
                 "autolink",
                 "lists",
@@ -40,17 +37,16 @@ export default function RTE({name, control, label, defaultValue =""}) {
                 "help",
                 "wordcount",
                 "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
-    )}
-    />
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
 
-     </div>
+    </div>
   )
 }
-
